f1e36aa15e72bee3989fc6535afa971a30d74e89
Ben Johnston <admiralackbar411@gmail.com>
2024-06-28 11:37:21 +0100

    Modified design for basket preparing for remove from cart button

--- src/Basket.jsx
+++ src/Basket.jsx
@@ -1,15 +1,19 @@
 export default function Basket({ cart }) {
   console.log(cart);
   return (
-    <div className="basket-wrapper">
+    <div className="basket-container">
       <div className="basket-header">
         <h3>Basket</h3>
+      </div>
+      <div className="basket-wrapper">
         {cart.map((item, index) => (
           <div key={index} className="basket-item">
-            <img src={item.url} alt={item.url} width={100} height={80} />
-            <p>{item.id}</p>
-            <p>{item.price}</p>
-            <p>Quantity: {item.quantity}</p>
+            <img src={item.url} alt={item.url} className="basket-item-image" />
+            <div className="basket-item-details">
+              <p className="basket-item-id">{item.id}</p>
+              <p className="basket-item-price">${item.price}</p>
+              <p className="basket-item-quantity">Quantity: {item.quantity}</p>
+            </div>
           </div>
         ))}
       </div>
